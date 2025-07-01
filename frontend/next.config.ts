7de58db245baa972f7b2762ffcaa6e8cf083ee54
Josh <josh_alot@protonmail.com>
2025-07-01 15:04:57 -0300

    redefining the frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['josh-alot.sh'], // future domain
-  },
+  /* config options here */
 };
 
 export default nextConfig;
